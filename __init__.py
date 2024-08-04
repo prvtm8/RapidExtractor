6ff4adb71abc5dacb7c3c8b1947f1931859e02bc
prvtm8 <prvtm8@protonmail.com>
2024-08-04 16:09:12 +0200

    Update __init__.py

---
+++ __init__.py
@@ -0,0 +1,16 @@
+# __init__.py
+
+"""
+RapidExtractor Package
+
+This package includes modules for various data extraction tasks, such as:
+- browser_history_extractor
+- cbc_extractor
+- dir_tree_extractor
+- installed_programs_extractor
+- prefetch_extractor
+- process_extractor
+- teamviewer_extractor
+
+Each module provides specific functionality for extracting and handling data.
+"""
